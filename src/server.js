775314913a4d01a6ae0081d901ff44c2a8ec93a2
Maria Fabris <mariaeduardafabris12@gmail.com>
2023-05-09 17:49:28 -0300

    Criação das rotas /user e /perdido

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import  express  from 'express'
 import { SERVER } from './config.js'
 import cors from 'cors'
 import adocaoRouter from './routes/adocaoRouter.js'
+import userRouter from './routes/userRouter.js'
+import perdidoRouter from './routes/perdidoRouter.js'
 
 const app = express()
 const port = SERVER.PORT
@@ -10,6 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/adocao', adocaoRouter)
+app.use('/user', userRouter)
+app.use('/perdido', perdidoRouter)
 
 app.all('*', (req, res) => {
   res.status(404).json( {message:  '404 Rota não encontrada...'})
