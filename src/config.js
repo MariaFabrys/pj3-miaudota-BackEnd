29e455e44676606fee7c96da6195a8866af5d525
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-06-28 15:25:16 -0300

    banco na nuvemmm

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ export const DB = {
     HOST: process.emv.DB_HOST || "localhost",
     USER: process.emv.DB_USER || "root",
     PASS: process.emv.DB_PASSWORD || "",
-    DB_NAME: process.emv.DB_DATABASE || "miaudota_db"
+    DB_NAME: process.emv.DB_NAME || "miaudota_db"
 }
